0581508e9a05421ba966338c41cd6387670a5242
Yash Kumar <121498975+YashKumar161@users.noreply.github.com>
2024-05-25 12:21:41 +0530

    Add files via upload

---
+++ Day 011 - Introduction to strings/main.py
@@ -0,0 +1,16 @@
+name = "Yash"
+friend = "Vish"
+batchmate = 'Dev'
+
+print("hello, " + name)
+
+triple = '''He said,
+i want to eat an apple
+hi Yash, how are you?'''
+print(triple)
+
+print(batchmate[2])
+
+print("lets se for loop\n")
+for character in triple:
+  print(character)
